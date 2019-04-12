0b60e54ccec3d47ea1c6ee560b59a0467a037339
nicole <nicole@cgitcayman.com>
2019-04-12 09:01:19 +0800

    Updates

---
+++ precache-manifest.13ee41e02d620ef8cb3014f62b24bc1c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "52085f99e7704477a6c7",
+    "url": "/ReactSimpleCalculator/static/js/runtime~main.ede41ce2.js"
+  },
+  {
+    "revision": "d65ca3b83c1652d69cdc",
+    "url": "/ReactSimpleCalculator/static/js/main.8f768aeb.chunk.js"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/ReactSimpleCalculator/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "d65ca3b83c1652d69cdc",
+    "url": "/ReactSimpleCalculator/static/css/main.10167e30.chunk.css"
+  },
+  {
+    "revision": "fd16aa2f471ecab5ac811c9ab532bf2d",
+    "url": "/ReactSimpleCalculator/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/React_Test/precache-manifest.2b76b1b1a8910f97de48a26c432878d5.js"
+  "/ReactSimpleCalculator/precache-manifest.13ee41e02d620ef8cb3014f62b24bc1c.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/React_Test/index.html", {
+workbox.routing.registerNavigationRoute("/ReactSimpleCalculator/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
